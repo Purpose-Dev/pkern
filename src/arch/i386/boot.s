a9f24050cc4c033cb7efa804d32fb39de49db68d
Riyane El Qoqui <riyane.dev@gmail.com>
2025-11-11 18:19:00 +0100

    refactor(boot): rewrite boot.asm in GAS syntax and update linker script for alignment

---
+++ src/arch/i386/boot.s
@@ -0,0 +1,33 @@
+.set ALIGN, 1 << 0
+.set MEMINFO, 1 << 1
+.set FLAGS, ALIGN | MEMINFO
+.set MAGIC, 0x1BADB002
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .bss
+.align 16
+stack_bottom:
+	.skip 16384 # 16KB
+stack_top:
+	# The ‘stack_top’ symbol is at the end of this area,
+	# that's where ESP needs to start.
+
+.section .text
+.global _start
+.type _start,@function
+_start:
+	movl $stack_top, %esp  # We point the stack pointer to ‘stack_top’, which we define in .bss
+	call kmain          # Now we can call Zig code
+	cli
+
+.halt:
+	hlt         # Use hlt to save CPU cycles
+	jmp .halt   # Infinite loop
+
+.size _start, . - _start
